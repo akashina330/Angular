97533b2fe6b1708c34fcd065e5ee4b6e61d42865
Anastasia Kashina <akashina330@gmail.com>
2020-11-01 18:41:37 +0300

    first

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,9 +1,13 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule
+      ],
       declarations: [
         AppComponent
       ],
@@ -16,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular1-Kashina'`, () => {
+  it(`should have as title 'MyMeeting'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular1-Kashina');
+    expect(app.title).toEqual('MyMeeting');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angular1-Kashina app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('MyMeeting app is running!');
   });
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MeetingListComponent } from './meeting-list/meeting-list.component';
+import { MeetingsAttendComponent } from './meetings-attend/meetings-attend.component';
+import { CreateMeetingComponent } from './create-meeting/create-meeting.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MeetingListComponent,
+    MeetingsAttendComponent,
+    CreateMeetingComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
