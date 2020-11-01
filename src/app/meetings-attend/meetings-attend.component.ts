66d8fffe79625a4f92f84fe94383c392c260cc21
Anastasia Kashina <akashina330@gmail.com>
2020-11-01 18:44:12 +0300

    first

---
+++ src/app/meetings-attend/meetings-attend.component.ts
@@ -0,0 +1,16 @@
+
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-meetings-attend',
+  templateUrl: './meetings-attend.component.html',
+  styleUrls: ['./meetings-attend.component.s\css']
+})
+export class MeetingsAttendComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
